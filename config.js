9a735e541b4896f5ebc8fcc22e3414b309ef577b
79mas <petravicius.tomas@gmail.com>
2026-09-21 13:18:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Public configuration. Never put a client secret, access token or password here. */
 window.ICS_CONFIG = {
-  googleClientId: '', // ...apps.googleusercontent.com (Web application OAuth client)
+  googleClientId: '209123294578-pn2klvgvoqa6np6jeun7iflrpfhr5fi5.apps.googleusercontent.com', // ...apps.googleusercontent.com (Web application OAuth client)
   urlReaderEndpoint: '', // https://your-reader.workers.dev/
   maxFileBytes: 10485760,
   maxPdfPages: 50
